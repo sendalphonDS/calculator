b11b6adf4a7c82dfb9924a30d5066a8bebe991c4
sendalphonDS <yor-el-20@hotmail.com>
2023-11-20 14:21:10 -0500

    commit5

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -8,11 +8,17 @@ import { Component } from '@angular/core';
 export class InicioComponent {
   edad = 25;
   peso = 60;
-
   altura = 170;
-
+  sexo = 'Masculino';
   cambiaraltura(event: any){
     this.altura = event.target.value;
+  }
+
+  masculino(){
+    this.sexo = 'Masculino';
+  }
+  femenino(){
+    this.sexo = 'Femenino';
   }
 
 }
